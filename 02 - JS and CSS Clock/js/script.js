4ec67a90cdd0628408ccfa72d9750892b04cfacd
Bartosz Bazański <bartosz.b@webinterpreter.com>
2017-02-08 13:16:16 +0100

    Remove transition on those clock hands that are at twelve

--- 02 - JS and CSS Clock/js/script.js
+++ 02 - JS and CSS Clock/js/script.js
@@ -5,15 +5,18 @@ const hourHand = document.querySelector('.hour-hand');
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360 + 90);
+    const secondsDegrees = ((seconds / 60) * 360);
+    secondHand.style.transition = secondsDegrees === 0 ? 'none' : 'all 0.05s';
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360 + 90);
+    const minutesDegrees = ((minutes / 60) * 360);
+    minuteHand.style.transition = minutesDegrees === 0 ? 'none' : 'all 0.05s';
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 12) * 360 + 90);
+    const hoursDegrees = ((hours / 12) * 360);
+    hourHand.style.transition = hoursDegrees === 0 ? 'none' : 'all 0.05s'; 
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
